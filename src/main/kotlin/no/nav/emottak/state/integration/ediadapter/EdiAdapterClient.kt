b8554e325496239c36ca7fa6bedfd4e31de37c8f
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2025-11-25 13:07:47 +0100

    Fjernet test endepunkt

--- src/main/kotlin/no/nav/emottak/state/integration/ediadapter/EdiAdapterClient.kt
+++ src/main/kotlin/no/nav/emottak/state/integration/ediadapter/EdiAdapterClient.kt
@@ -7,7 +7,6 @@ import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
@@ -109,7 +108,6 @@ class EdiAdapterClient(
     }
 
     private suspend inline fun <reified T> handleResponse(httpResponse: HttpResponse): Pair<T?, ErrorMessage?> {
-        log.info("EDI2 test: Response from edi-adapter: ${httpResponse.bodyAsText()}")
         return if (httpResponse.status == HttpStatusCode.OK || httpResponse.status == HttpStatusCode.Created) {
             Pair(httpResponse.body(), null)
         } else {
