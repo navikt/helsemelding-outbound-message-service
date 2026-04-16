086bd14962ddb9a0be548a8966bbfc0d8d4b2d0e
Kristian Frøhlich <186453+kfh@users.noreply.github.com>
2026-04-16 09:12:36 +0200

    Added back oid to apprec errors (#84)

--- src/main/kotlin/no/nav/helsemelding/outbound/model/AppRecErrorMessage.kt
+++ src/main/kotlin/no/nav/helsemelding/outbound/model/AppRecErrorMessage.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class AppRecErrorMessage(
     val code: String? = null,
-    val text: String? = null
+    val description: String? = null,
+    val oid: String? = null
 )
